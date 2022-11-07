fa44ccfeac4f2b017f8b4b7bbe15650aa2e37eec
Jenn Guneratne <jenn.guneratne@gmail.com>
2022-11-06 21:11:13 -0500

    Create global variables

---
+++ script.js
@@ -0,0 +1,10 @@
+// Global variables, targeting canvas, grid and buttons
+
+let canvas = document.getElementById('grid');
+let guide = document.getElementById('guide');
+let colorPicker = document.getElementById('color-picker');
+let toggleGuide = document.getElementById('toggle-guide');
+let clearBtn = document.getElementById('clear-btn');
+
+let ctx = canvas.getContext('2d');
+
