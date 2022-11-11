bd6ee98c792ccedd509f95fab60840ee546c113a
Jenn Guneratne <jenn.guneratne@gmail.com>
2022-11-11 15:25:45 -0500

    Remake grid over canvas with flexbox

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 // Global variables, targeting canvas, guides and buttons
 
 let canvas = document.getElementById('canvas');
-let guide = document.getElementById('guide');
+let guideLines = document.getElementById('guide');
 let colorPicker = document.getElementById('color-picker');
 let toggleGuide = document.getElementById('toggleOnOff');
 let clearBtn = document.getElementById('clear-btn');
@@ -13,40 +13,59 @@ let clearBtn = document.getElementById('clear-btn');
 let ctx = canvas.getContext('2d');
 
 // Initialize canvas background default color
-
 ctx.fillStyle = '#ffffff';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 // Set dimensions for grid
-
-let cellCount = 16; 
-let cellPixelLength = canvas.width / cellCount; 
-
-
+let cellCount = 16;
 
 // Functions
 
 function makeGrid() {
 
-    // Setup the guides
+    let columns = cellCount;
+    let rows = cellCount;
 
-    guide.style.width = `${canvas.width}px`;
-    guide.style.height = `${canvas.height}px`;
 
-    guide.style.gridTemplateColumns = `repeat(${cellCount}, 1fr)`;
-    guide.style.gridTemplateRows = `repeat(${cellCount}, 1fr)`;
+    //Setup the guide lines
+    for(let i = 0; i < columns; i++) {
+        let column = document.createElement('div');
+        column.classList.add('column');
+        guideLines.appendChild(column);
 
-    [...Array(cellCount **2)].forEach(() => guide.insertAdjacentHTML("beforeend", "<div></div>"));
+        for(let j = 0; j < rows; j++) {
+            let row = document.createElement('div');
+            row.classList.add('row');
+            column.appendChild(row);
+        }
+        
+    }
 
 }
 
+//  function rangeSliderControl() {
+//     let slider = document.getElementById('grid-range');
+//     let output = document.getElementById('grid-size');
 
-function toggleGuideSwitch() {
-    guide.style.display = toggleGuide.checked ? null : "none";
-}
+//     output.innerHTML = slider.value; 
+
+//     slider.oninput = function() {
+//         output.innerHTML = this.value;
+//     }
+
+// }
+
+
+// function toggleGuideSwitch() {
+//     guide.style.display = toggleGuide.checked ? null : "none";
+// }
 
 // Event Listeners
 
-guide.addEventListener('change', toggleGuideSwitch);
+//guide.addEventListener('change', toggleGuideSwitch);
+
+
+// Call functions
 
 makeGrid();
+//rangeSliderControl();
