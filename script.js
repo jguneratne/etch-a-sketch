5939faa7be391f68229aa4fe4b46a22e8a22535e
Jenn Guneratne <jenn.guneratne@gmail.com>
2022-11-21 09:10:59 -0500

    Begin adding draw features for canvas

--- script.js
+++ script.js
@@ -6,7 +6,9 @@
 
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
+let drawing = false; 
 let guideLines = document.getElementById('guide');
+let slider = document.getElementById('grid-range');
 let colorPicker = document.getElementById('color-picker');
 let toggleGuide = document.getElementById('toggleOnOff');
 let clearBtn = document.getElementById('clear-btn');
@@ -39,7 +41,7 @@ function makeGrid(cellCount) {
 
 
  function rangeSliderControl() {
-    let slider = document.getElementById('grid-range');
+    // let slider = document.getElementById('grid-range');
     let output = document.getElementById('grid-value');
     let cellCount = slider.value;
 
@@ -61,13 +63,67 @@ function makeGrid(cellCount) {
 }
 
 
+function startDrawing(e) {
+
+    // Ensure user is using primary mouse button
+    if (e.button !== 0) {
+        return;
+    }
+
+    drawing = true;
+    ctx.beginPath();  // Allows user to make new line after lifting mouse and clicking again
+    draw(e);
+}
+
+function endDrawing() {
+    drawing = false;
+
+}
+
+function getMousePos(canvas, evt) {
+    let rect = canvas.getBoundingClientRect(),
+      scaleX = canvas.width / rect.width,
+      scaleY = canvas.height / rect.height;
+  
+    return {
+      x: (evt.clientX - rect.left) * scaleX,
+      y: (evt.clientY - rect.top) * scaleY
+    }
+}
+
+
+function draw(e) {
+    if(!drawing) return; 
+
+    let { x, y } = getMousePos(canvas, e);
+
+    ctx.lineWidth = 10;
+    ctx.lineCap = "round";
+
+    ctx.lineTo(x, y);    
+    ctx.strokeStyle = colorPicker.value;
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+}
+
+
 // function toggleGuideSwitch() {
-//     guide.style.display = toggleGuide.checked ? null : "none";
+//     guideLines.style.display = toggleGuide.checked ? null : "none";
 // }
+
+
+
 
 // Event Listeners
 
-//guide.addEventListener('change', toggleGuideSwitch);
+guideLines.addEventListener('change', toggleGuideSwitch);
+
+canvas.addEventListener('mousedown', startDrawing);
+
+canvas.addEventListener('mouseup', endDrawing);
+
+// canvas.addEventListener('mousemove', draw); 
 
 
 // CALL FUNCTIONS
