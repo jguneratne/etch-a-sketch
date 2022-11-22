e76c9132283c65929e71852d5cf9a2295769a14a
Jenn Guneratne <jenn.guneratne@gmail.com>
2022-11-22 17:53:36 -0500

    Create eventListener for mouseover to draw color

--- script.js
+++ script.js
@@ -4,11 +4,8 @@
 
 // GLOBAL VARIABLES (target canvas, guides and buttons)
 
-let canvas = document.getElementById('canvas');
-let ctx = canvas.getContext('2d');
-let drawing = false; 
+let canvas = document.getElementsByClassName('.canvas-div');
 let guideLines = document.getElementById('guide');
-let slider = document.getElementById('grid-range');
 let colorPicker = document.getElementById('color-picker');
 let toggleGuide = document.getElementById('toggleOnOff');
 let clearBtn = document.getElementById('clear-btn');
@@ -21,10 +18,6 @@ function makeGrid(cellCount) {
     let columns = cellCount;
     let rows = cellCount;
 
-    // Initialize canvas background default color
-    ctx.fillStyle = '#ffffff';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-
     //Setup the guide lines
     for(let i = 0; i < columns; i++) {
         let column = document.createElement('div');
@@ -34,6 +27,7 @@ function makeGrid(cellCount) {
         for(let j = 0; j < rows; j++) {
             let row = document.createElement('div');
             row.classList.add('row');
+            row.addEventListener('mouseover', function() {row.style.background = colorPicker.value;});
             column.appendChild(row);
         }
     }
@@ -41,7 +35,7 @@ function makeGrid(cellCount) {
 
 
  function rangeSliderControl() {
-    // let slider = document.getElementById('grid-range');
+    let slider = document.getElementById('grid-range');
     let output = document.getElementById('grid-value');
     let cellCount = slider.value;
 
@@ -63,70 +57,45 @@ function makeGrid(cellCount) {
 }
 
 
-function startDrawing(e) {
 
-    // Ensure user is using primary mouse button
-    if (e.button !== 0) {
-        return;
-    }
+// function colorPickerFill() {
 
-    drawing = true;
-    ctx.beginPath();  // Allows user to make new line after lifting mouse and clicking again
-    draw(e);
-}
+//     let cellDraw = document.querySelector('div.row');
+    
+//     guideLines.addEventListener('mousedown', function(e) {
+//         if (e.target.matches('div.row')) {
+//             cellDraw.style.background = colorPicker.value;
+//         }
+//     })
 
-function endDrawing() {
-    drawing = false;
-
-}
-
-function getMousePos(canvas, evt) {
-    let rect = canvas.getBoundingClientRect(),
-      scaleX = canvas.width / rect.width,
-      scaleY = canvas.height / rect.height;
-  
-    return {
-      x: (evt.clientX - rect.left) * scaleX,
-      y: (evt.clientY - rect.top) * scaleY
-    }
-}
-
-
-function draw(e) {
-    if(!drawing) return; 
-
-    let { x, y } = getMousePos(canvas, e);
-
-    ctx.lineWidth = 10;
-    ctx.lineCap = "round";
-
-    ctx.lineTo(x, y);    
-    ctx.strokeStyle = colorPicker.value;
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.moveTo(x, y);
-}
-
-
-// function toggleGuideSwitch() {
-//     guideLines.style.display = toggleGuide.checked ? null : "none";
+//     guideLines.addEventListener('mousemove', function(e) {
+//         if (e.target.matches ('div.row')) {
+//             cellDraw.style.background = colorPicker.value;
+//         }
+//     })
 // }
+
+
+
+
+//function toggleGuideSwitch() {
+//     div.guideLines.style.display = toggleGuide.checked ? null : "none";
+
+    //div.column.toggle('remove');
+    //div.row.toggle('remove');
+//}
 
 
 
 
 // Event Listeners
 
-guideLines.addEventListener('change', toggleGuideSwitch);
+//guideLines.addEventListener('change', toggleGuideSwitch);
 
-canvas.addEventListener('mousedown', startDrawing);
-
-canvas.addEventListener('mouseup', endDrawing);
-
-// canvas.addEventListener('mousemove', draw); 
 
 
 // CALL FUNCTIONS
 
 makeGrid();
 rangeSliderControl();
+//colorPickerFill();
